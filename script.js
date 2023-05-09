01a70d301c7a6a2f55db9758701004ce98e4057c
Rodrigo Silva <rodrigob_silva@hotmail.com>
2023-05-09 15:26:24 -0400

    Adjust quiz2 page

--- script.js
+++ script.js
@@ -56,28 +56,28 @@ document.getElementById('quizForm').addEventListener('submit', function(event) {
     const question3 = document.querySelector('input[name="question3"]:checked');
     
     // Display advice based on selected answers
-    let result = 'Here is some advice based on your answers:';
+    let result = `<h2>Here is some advice based on your answers:</h2>`;
     result += '<br>';
     
     if (question1) {
-      result += 'Answer for question 1: ' + question1.value + '<br>';
+      result += '<strong>Answer for question 1: </strong>' + question1.value + '<br>';
       // Add advice or information based on the selected question1
     } else {
-      result += 'No question selected<br>';
+      result += "<em>Question 1 wasn't selected</em><br>";
     }
     
     if (question2) {
-      result += 'Answer for question 2: ' + question2.value + '<br>';
+      result += '<strong>Answer for question 2: </strong>' + question2.value + '<br>';
       // Add advice or information based on the selected question2
     } else {
-      result += 'No question selected<br>';
+      result += "<em>Question 2 wasn't selected</em><br>";
     }
     
     if (question3) {
-      result += 'Answer for question 3: ' + question3.value + '<br>';
+      result += '<strong>Answer for question 3: </strong>' + question3.value + '<br>';
       // Add advice or information based on the selected question3
     } else {
-      result += 'No question selected<br>';
+      result += "<em>Question 3 wasn't selected</em><br>";
     }
     
     document.getElementById('result').innerHTML = result;
